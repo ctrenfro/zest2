5f917afa6f091ddd0084514e45455cc00a2df84e
Christopher Renfro <ctrenfro22@gmail.com>
2024-04-15 12:12:41 -0400

    pathing

--- server/src/index.js
+++ server/src/index.js
@@ -26,7 +26,7 @@ connectDB();
 
 app.get("/*", function (req, res) {
   res.sendFile(
-    path.resolve("../recipebuilder/build/index.html"),
+    path.resolve("../../recipebuilder/build/index.html"),
     function (err) {
       if (err) {
         res.status(500).send(err);
